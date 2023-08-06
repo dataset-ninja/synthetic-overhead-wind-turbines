1b8d5344dab3986a019f3aee019a2790e75ca4f4
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-06 14:26:04 +0600

    upd

--- src/settings.py
+++ src/settings.py
@@ -24,7 +24,7 @@ PROJECT_NAME_FULL: str = (
 LICENSE: License = License.CC_BY_4_0()
 
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Energy(is_used=False)]
-CATEGORY: Category = Category.EnergyAndUtilities(extra=Category.Aerial())
+CATEGORY: Category = Category.EnergyAndUtilities(extra=[Category.Aerial(), Category.Satellite()], is_original_dataset=False)
 
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
